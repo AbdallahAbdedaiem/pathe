feb0f2e4b413e589e290fe4f9e20c124e7285aef
Abdallah Abdedaiem <abdallah.abdeddayem@gmail.com>
2024-07-25 15:07:29 +0100

    setup data layer

---
+++ src/main/java/org/tarkiba/pathe/api/FilmResource.java
@@ -0,0 +1,18 @@
+package org.tarkiba.pathe.api;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class FilmResource {
+
+    @GET
+    @Path("/hello-world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello World!";
+    }
+}
